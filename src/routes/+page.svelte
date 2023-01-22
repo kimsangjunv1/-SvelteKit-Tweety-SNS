ec4eea3fb1a17e2ae33c4c821d140327cd36b2db
kimsangjunv1 <ingyoe64@gmail.com>
2023-01-23 03:23:00 +0900

    기초 뼈대 완성

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,16 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import { onDestroy, onMount } from 'svelte';
+	import { page } from '$app/stores';
+
+	onMount(() => {
+		console.log('마운트(로딩) 되었습니다.');
+	});
+	onDestroy(() => {
+		console.log('언마운트(사라짐) 되었습니다.');
+	});
+</script>
+
+<h1>hi</h1>
+<pre>
+    {JSON.stringify($page, null, 2)}
+</pre>
---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,1 @@
+<h1>hi im about</h1>
