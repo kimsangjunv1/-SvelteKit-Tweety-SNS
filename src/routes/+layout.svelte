4643873a3224a36dc8e4fe3c3b656799509ef82b
kimsangjunv1 <ingyoe64@gmail.com>
2023-01-23 19:06:53 +0900

    인트로 페이지 생성

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,4 +1,9 @@
-<a href="/">home</a>
-<a href="/about">about</a>
+<!-- 파일명에 __ 가 붙어있을경우 layout파일이 무시된다? -->
+<script lang="ts">
+	import '$root/styles/global.css'
+</script>
+
+<!-- <a href="/">home</a>
+<a href="/about">about</a> -->
 
 <slot />
