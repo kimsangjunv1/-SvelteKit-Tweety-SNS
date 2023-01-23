8dbc7bfdd5ee988748b3d96802491650eb25482c
kimsangjunv1 <ingyoe64@gmail.com>
2023-01-24 00:43:09 +0900

    네비게이션 스타일 수정

--- src/components/navigation.svelte
+++ src/components/navigation.svelte
@@ -45,3 +45,66 @@
 		<button class="btn tweet">업로드</button>
 	</div>
 </aside>
+
+<style>
+	.container {
+		position: sticky;
+		top: 0;
+	}
+
+	ul {
+		display: grid;
+		gap: var(--spacing-8);
+		font-size: var(--font-18);
+	}
+
+	li a {
+		display: inline-flex;
+		align-items: center;
+		gap: var(--spacing-16);
+		padding: 1.4rem;
+		border-radius: var(--radius-base);
+		transition: all 0.2s ease;
+	}
+
+	li a:hover {
+		background-color: var(--color-link-hover);
+	}
+
+	li.active {
+		font-weight: 700;
+	}
+
+	.logo {
+		font-size: var(--font-32);
+	}
+
+	span {
+		display: none;
+	}
+
+	.tweet {
+		display: none;
+		text-align: center;
+	}
+
+	.tweet:hover {
+		background-color: var(--color-btn-primary-active-hover);
+	}
+
+	@media (min-width: 1024px) {
+		aside {
+			padding: 0 var(--spacing-32);
+		}
+
+		span {
+			display: block;
+		}
+
+		.tweet {
+			width: 100%;
+			display: block;
+			margin-top: var(--spacing-16);
+		}
+	}
+</style>
