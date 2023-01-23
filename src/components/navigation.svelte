23efb855eca06a9cd565c88f80b68697374249c5
kimsangjunv1 <ingyoe64@gmail.com>
2023-01-23 20:03:21 +0900

    네비게이션 구성

---
+++ src/components/navigation.svelte
@@ -0,0 +1,47 @@
+<script lang="ts">
+	import { page } from '$app/stores'
+	import Icon from '$root/components/icon.svelte'
+
+	// 활성화 된 링크가 무엇인지 파악하기 위한 용도
+	$: path = $page.url.pathname
+</script>
+
+<aside>
+	<div class="container">
+		<nav>
+			<ul>
+				<li>
+					<a href="/" class="logo">bird</a>
+				</li>
+				<li class:active={path === '/home'}>
+					<a href="/home">
+						<Icon width="32" height="32" name="home" />
+						<span>Home</span>
+					</a>
+				</li>
+
+				<li class:active={path === '/home/profile/matia'}>
+					<a href="/home/profile/matia">
+						<Icon width="32" height="32" name="profile" />
+						<span>Profile</span>
+					</a>
+				</li>
+
+				<li class:active={path === '/home/settings'}>
+					<a href="/home/settings">
+						<Icon width="32" height="32" name="settings" />
+						<span>settings</span>
+					</a>
+				</li>
+
+				<li class:active={path === '/home/about'}>
+					<a href="/home/about">
+						<Icon width="32" height="32" name="about" />
+						<span>about</span>
+					</a>
+				</li>
+			</ul>
+		</nav>
+		<button class="btn tweet">tweet</button>
+	</div>
+</aside>
---
+++ src/routes/home/+page.svelte
@@ -0,0 +1,20 @@
+<script>
+	// import Icon from '$root/components/icon.svelte'
+</script>
+
+<svelte:head>
+	<title>Home</title>
+</svelte:head>
+
+<h1>Feed</h1>
+
+<!-- <Icon name="about" /> -->
+<style>
+	h1 {
+		position: sticky;
+		top: 0;
+		padding: var(--spacing-8) var(--spacing-24);
+		font-size: var(--font-24);
+		backdrop-filter: blur(100px);
+	}
+</style>
