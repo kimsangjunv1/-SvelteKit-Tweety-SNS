eec585ec81bd8420048878685bc0135c84a70659
kimsangjunv1 <ingyoe64@gmail.com>
2023-01-23 19:39:20 +0900

    에러 페이지 생성

---
+++ src/routes/+error.svelte
@@ -0,0 +1,47 @@
+<script context="module" lang="ts">
+	// import type { ErrorLoad } from '@sveltejs/kit'
+	import { page } from '$app/stores'
+
+	// export const load: ErrorLoad = async ({
+	// 	error,
+	// 	status
+	// }) => {
+	// 	return {
+	// 		props: {
+	// 			title: `${status}: ${error.message}`
+	// 		}
+	// 	}
+	// }
+</script>
+
+<script lang="ts">
+	export let title: string
+</script>
+
+<div class="error">
+	<!-- 페이지에 에러가 있다면 메세지를 띄워라! -->
+	<h1>으! {$page?.status} {$page.error?.message}</h1>
+	<pre>
+        <!-- JSON파일을 4칸씩 띄워서 표시해라! -->
+        {JSON.stringify($page, null, 4)}
+    </pre>
+	<!-- <h1>{title}</h1> -->
+	<img src="/error.webp" alt="Surprised cat" />
+</div>
+
+<style>
+	.error {
+		height: 100vh;
+		display: grid;
+		gap: var(--spacing-32);
+		place-content: center;
+		place-items: center;
+	}
+
+	img {
+		width: 200px;
+		height: 200px;
+		border-radius: 50%;
+		object-fit: cover;
+	}
+</style>
